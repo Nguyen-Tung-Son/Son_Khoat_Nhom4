b6dcc6bc20e7633f7b4e43a79cd091c921248a59
khoatngguyen <nguyenvankkhoat2000@gmail.com>
2022-12-05 00:36:02 +0700

    fistcommit

---
+++ Models/CategoryProduct.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace nhom4.Models;
+
+public class CategoryProduct {
+     
+     public string ProductID { get; set; }
+
+     public string ProductName { get; set; }
+
+     public string Information { get; set; }
+     
+     
+}
